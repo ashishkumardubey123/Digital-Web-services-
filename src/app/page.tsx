e03f7255579ad259bd3a11c7759ccb90b6fd375e
ashishkumardubey123 <ashish9039062705@gmail.com>
2025-12-24 23:13:45 +0530

    fix: correct className syntax in main tag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import Our_services from "@/Components/Our_services";
 export default function Home() {
   return (
       
-<main className="min-h-screen bg-black/[0.96] antialiased  bg-grid-white/[0.02]">
+<main className="min-h-screen bg-black/[0.96]89 antialiased  bg-grid-white/[0.02]">
      
         <Herosection/>
         <Our_services/>
