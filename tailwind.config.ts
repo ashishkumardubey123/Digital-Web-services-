20a88e04dba2b560d970574d380c89b4129b0a4b
ashishkumardubey123 <ashish9039062705@gmail.com>
2025-12-28 12:33:30 +0530

    fix: correct casing in components directory path in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,7 +13,7 @@ module.exports = {
   content: [
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/Components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   darkMode: "class",
   theme: {
